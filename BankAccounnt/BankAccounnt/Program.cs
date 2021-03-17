81c5d348bf527ffbd70f66cac72188c09a0925c6
Alif-Utsho <ashahriar29@gmail.com>
2021-03-17 13:21:01 +0600

    Polymorphism task (abstract)

---
+++ BankAccounnt/BankAccounnt/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounnt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account alif = new Savings("Alif", "001", 5000.0f);
+            Account utsho = new Current("Utsho", "002", 2000f);
+            Account rakib = new Fixed("Rakib", "003", 1000f, 2015, 10);
+            
+            Account ss = new SuperSavings("Shahriar", "004", 100f);
+            ss.Withdraw(80);
+            ss.ShowInfo();
+
+            Account overdraft = new Overdraft("Arif", "005", 1000f, 5000);
+            overdraft.Transfer(ss, 5000);
+            overdraft.ShowInfo();
+
+            Customer customer = new Customer("Alif Uthso");
+
+            //alif.ShowInfo();
+            //alif.Deposit(2000f);
+            //alif.ShowInfo();
+            //alif.Withdraw(3000);
+            //alif.ShowInfo();
+
+            //rakib.Deposit(5000);
+            //rakib.Withdraw(1000);
+            //rakib.ShowInfo();
+
+            //alif.Transfer(utsho, 1000f);
+            //utsho.ShowInfo();
+
+            //customer.AddAccount(alif, utsho);
+            //customer.AccDetails();
+        }
+    }
+}
